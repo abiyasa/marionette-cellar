767c0f79f0331f56879a0e9c490ad8e7b12cb82d
abiyasa <ilham.suhardi@gmail.com>
2012-09-25 16:50:32 +0200

    add jslint comment

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,6 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, forin: true, maxerr: 50, regexp: true */
+/*global console, window, Backbone */
+
 // Models
 window.Wine = Backbone.Model.extend({
     urlRoot:"../api/wines",
